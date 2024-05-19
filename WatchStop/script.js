0a08062c612e17626af834687d9a93478183a79c
Maxim Nechistyak <MaximNechistyak@gmail.com>
2024-05-19 20:30:57 +0300

    Воспроизведение звука

--- WatchStop/script.js
+++ WatchStop/script.js
@@ -1,103 +1,97 @@
-// Progressbar 
-		let progressBar = document.querySelector('.progress-bar');
-		let progress = 0;
-		let progressBarid = setInterval(0, 60);
-        class Stopwatch {
-            constructor(display, results) {
-                this.running = false;
-                this.display = display;
-                this.results = results;
-                this.laps = [];
-                this.reset();
-                this.print(this.times);
-            }
 
-            reset() {
-                this.times = [0, 0, 0];
-				progress = 0;
-            }
+    let progressBar = document.querySelector('.progress-bar');
+    let progress = 0;
+    class Stopwatch {
+        constructor(display, results) {
+            this.running = false;
+            this.display = display;
+            this.reset();
+            this.print(this.times);
+        }
 
-            start() {
-                if (!this.time) this.time = performance.now();
-                if (!this.running) {
-                    this.running = true;
-                    requestAnimationFrame(this.step.bind(this));
-                }
-            }
+        reset() {
+            this.times = [0, 0, 0];
+            progress = 0;
+        }
 
-            lap() {
-                let times = this.times;
-                const badge = document.createElement('span');
-                badge.classList.add('badge', 'bg-primary', 'm-1');
-                badge.textContent = this.format(times);
-                badgeContainer.appendChild(badge);
-
-            }
-
-            stop() {
-                this.running = false;
-                this.time = null;
-            }
-
-            restart() {
-                if (!this.time) this.time = performance.now();
-                if (!this.running) {
-                    this.running = true;
-                    requestAnimationFrame(this.step.bind(this));
-                }
-                this.reset();
-            }
-
-            clear() {
-                badgeContainer.innerHTML = '';
-            }
-
-            step(timestamp) {
-                if (!this.running) return;
-                this.calculate(timestamp);
-                this.time = timestamp;
-                this.print();
+        start() {
+            if (!this.time) this.time = performance.now();
+            if (!this.running) {
+                this.running = true;
                 requestAnimationFrame(this.step.bind(this));
-				// Progressbar
-				
-            }
-
-            calculate(timestamp) {
-                var diff = timestamp - this.time;
-                // Hundredths of a second are 100 ms
-                this.times[2] += diff / 10;
-                // Seconds are 100 hundredths of a second
-                if (this.times[2] >= 100) {
-                    this.times[1] += 1;
-                    this.times[2] -= 100;
-
-					progress += 1;
-					progressBar.style.width = `${progress}%`;
-                	progressBar.setAttribute('aria-valuenow', progress);
-                }
-                // Minutes are 60 seconds
-                if (this.times[1] >= 60) {
-                    this.times[0] += 1;
-                    this.times[1] -= 60;
-                }
-            }
-
-            print() {
-                this.display.innerText = this.format(this.times);
-            }
-
-            format(times) {
-                return `${pad0(times[0], 2)}:${pad0(times[1], 2)}:${pad0(Math.floor(times[2]), 2)}`;
             }
         }
 
-        function pad0(value, count) {
-            var result = value.toString();
-            for (; result.length < count; --count)
-                result = '0' + result;
-            return result;
+        lap() {
+            let times = this.times;
+            const badge = document.createElement('span');
+            badge.classList.add('badge', 'bg-primary', 'm-1', 'fs-3');
+            badge.textContent = this.format(times);
+            badgeContainer.appendChild(badge);
+
         }
 
-        let stopwatch = new Stopwatch(
-            document.querySelector('.stopwatch'),
-            document.querySelector('.results'));
+        stop() {
+            this.running = false;
+            this.time = null;
+        }
+
+        restart() {
+            if (!this.time) this.time = performance.now();
+            if (!this.running) {
+                this.running = true;
+                requestAnimationFrame(this.step.bind(this));
+            }
+            this.reset();
+        }
+
+        clear() {
+            badgeContainer.innerHTML = '';
+        }
+
+        step(timestamp) {
+            if (!this.running) return;
+            this.calculate(timestamp);
+            this.time = timestamp;
+            this.print();
+            requestAnimationFrame(this.step.bind(this));
+        }
+
+        calculate(timestamp) {
+            var diff = timestamp - this.time;
+            // Hundredths of a second are 100 ms
+            this.times[2] += diff / 10;
+            // Seconds are 100 hundredths of a second
+            if (this.times[2] >= 100) {
+                this.times[1] += 1;
+                this.times[2] -= 100;
+
+                progress += 1;
+                progressBar.style.width = `${progress}%`;
+                progressBar.setAttribute('aria-valuenow', progress);
+            }
+            // Minutes are 60 seconds
+            if (this.times[1] >= 60) {
+                this.times[0] += 1;
+                this.times[1] -= 60;
+            }
+        }
+
+        print() {
+            this.display.innerText = this.format(this.times);
+        }
+
+        format(times) {
+            return `${pad0(times[0], 2)}:${pad0(times[1], 2)}:${pad0(Math.floor(times[2]), 2)}`;
+        }
+    }
+
+    function pad0(value, count) {
+        var result = value.toString();
+        for (; result.length < count; --count)
+            result = '0' + result;
+        return result;
+    }
+
+    let stopwatch = new Stopwatch(
+        document.querySelector('.stopwatch'));
